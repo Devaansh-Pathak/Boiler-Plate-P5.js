a7df58ada3857596d10a3a701cb15dc22d4d9a47
Devaansh-Pathak <75683336+Devaansh-Pathak@users.noreply.github.com>
2020-12-17 19:56:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,27 +6,27 @@ function setup() {
   fixedRect.shapeColor="green";
   movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor="green";
-
+  movingRect.velocityX =-2;
+  fixedRect.velocityX = 2;
 }
 
 function draw() {
   background(0); 
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-
+  
   if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&
-    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 &&
-    movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
-    fixedRect.y - movingRect.y< movingRect.height/2 + fixedRect.height/2){
-      
-    fixedRect.shapeColor="red";
-    movingRect.shapeColor="red";
+  fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2)
+  {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
+
   }
-  else{
-    fixedRect.shapeColor="green";
-    movingRect.shapeColor="green";
+
+  if(movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
+  fixedRect.y - movingRect.y< movingRect.height/2 + fixedRect.height/2)
+  {
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
   }
-  //console.log(movingRect.x - fixedRect.x)
  
   drawSprites();
 }
